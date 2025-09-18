51876bd8431b2ccf7090c79a6a90db622bbb7d82
Pulkit Porwal <pulkitporwal.brn@gmail.com>
2025-09-18 20:27:54 +0530

    add: Padding in the the Hero Text

--- components/hero.jsx
+++ components/hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
   return (
     <div className={`w-full min-h-[50vh] md:min-h-[60vh] lg:min-h-[70vh] bg-[url(/bg.svg)] bg-cover bg-no-repeat bg-center flex items-center justify-center ${montserrat_alt.className}`}>
         <div className='px-4 sm:px-6 flex items-center flex-col gap-y-4 md:gap-y-6'>
-            <h1 className='font-bold text-2xl md:text-3xl lg:text-4xl text-center text-[#005B96]'>International Conference on <br />Intelligent Systems and Emerging Technologies - (ICISET 2026)</h1>
+            <h1 className='font-bold text-2xl md:text-3xl lg:text-3xl text-center text-[#005B96] px-10'>International Conference on <br />Intelligent Systems and Emerging Technologies - (ICISET 2026)</h1>
             <span className='text-center text-lg md:text-xl text-[#005B96]'>Organised By</span>
             <h2 className='font-semibold text-2xl md:text-3xl text-center text-[#005B96]'>Amity University Jaipur, Rajasthan</h2>
             <h2 className='font-semibold text-xl md:text-2xl text-center text-[#005B96]'>20<sup>th</sup> & 21<sup>st</sup> April, 2026</h2>
--- components/important-dates.jsx
+++ components/important-dates.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const ImportantDates = () => {
   const dates = [
     { label: "Call for Papers", date: "5th July, 2025", highlight: true },
-    { label: "Acceptance Notification", date: "1st December, 2025", highlight: false },
-    { label: "Last Date for Camera Ready", date: "16th December, 2025", highlight: false },
-    { label: "Last Date of Registration", date: "16th December, 2025", highlight: true },
-    { label: "Conference Dates", date: "23rd - 24th December, 2025", highlight: false },
+    { label: "Last Date (Paper Submission)", date: "15th January, 2026", highlight: true },
+    { label: "Acceptance Notification", date: "15th February, 2026", highlight: false },
+    { label: "Last Date for Camera Ready", date: "10th March, 2026", highlight: false },
+    { label: "Last Date of Registration", date: "10th March, 2026", highlight: true },
+    { label: "Conference Dates", date: "23rd - 24th April, 2026", highlight: false },
   ];
+
 
   return (
     <div className="px-3 py-5 min-h-[350px] max-w-4xl mx-auto border rounded-xl ">
@@ -21,9 +23,8 @@ const ImportantDates = () => {
             {dates.map((item, idx) => (
               <tr
                 key={idx}
-                className={`text-lg ${
-                  idx % 2 === 0 ? "bg-gray-100" : "bg-white"
-                }`}
+                className={`text-lg ${idx % 2 === 0 ? "bg-gray-100" : "bg-white"
+                  }`}
               >
                 <td className="px-3 py-2 font-medium text-gray-700 border-b border-gray-200 flex items-center gap-2">
                   {item.label}
