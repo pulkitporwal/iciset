807c6b07d39ea90f49d35ee962d4e01af2834f50
Pulkit Porwal <pulkitporwal.brn@gmail.com>
2025-09-19 21:32:28 +0530

    Changes4

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -17,101 +17,98 @@ const ContactUs = () => {
                 <div className="flex justify-center items-center flex-col gap-8 mt-6">
                     <div className="p-6 rounded-2xl shadow space-y-4">
                         <h3 className="text-lg font-semibold">Contact Details</h3>
-                        <div class="grid md:grid-cols-2 gap-6 p-6 bg-gray-50 rounded-2xl shadow-lg">
-                    
-                            <div class="p-5 bg-white rounded-xl shadow-md hover:shadow-lg transition">
-                                <h2 class="text-xl font-semibold text-[#005B96] mb-2">
-                                    Amity University Jaipur Campus
-                                </h2>
-                                <p class="text-gray-600 leading-relaxed">
-                                    SP-1, Kant Kalwar, RIICO Industrial Area, NH-11C, Jaipur, Rajasthan <br />
-                                    <span class="font-medium text-gray-800">Tel:</span> 01426-405678
-                                    <span class="text-sm text-gray-500">(University Board Number - For general queries only)</span>
-                                </p>
-                            </div>
-                            <div class="p-5 bg-white rounded-xl shadow-md hover:shadow-lg transition">
-                                <h2 class="text-xl font-semibold text-[#005B96] mb-2">
-                                    Amity University Jaipur City Office
-                                </h2>
-                                <p class="text-gray-600 leading-relaxed">
-                                    Amity House, C - 119, Lal Kothi Scheme, Behind Vidhan Sabha, Jaipur - 302 015 (Rajasthan) <br />
-                                    <span class="font-medium text-gray-800">Tel:</span> 0141-4735000, 2744350
-                                </p>
-                            </div>
-                        </div>
-
-<form
-                        onSubmit={handleSubmit}
-                        className="space-y-4 bg-gray-50 p-6 rounded-2xl shadow"
-                    >
-                        <div>
-                            <label className="block text-sm font-medium mb-1">Name</label>
-                            <input
-                                type="text"
-                                name="name"
-                                className="w-full px-3 py-2 border rounded-md focus:ring focus:ring-blue-500"
-                                placeholder="Your Name"
-                                required
-                            />
-                        </div>
-
-                        <div>
-                            <label className="block text-sm font-medium mb-1">Email</label>
-                            <input
-                                type="email"
-                                name="email"
-                                className="w-full px-3 py-2 border rounded-md focus:ring focus:ring-blue-500"
-                                placeholder="Your Email"
-                                required
-                            />
-                            <ValidationError
-                                prefix="Email"
-                                field="email"
-                                errors={state.errors}
-                            />
-                        </div>
-
-                        <div>
-                            <label className="block text-sm font-medium mb-1">Subject</label>
-                            <input
-                                type="text"
-                                name="subject"
-                                className="w-full px-3 py-2 border rounded-md focus:ring focus:ring-blue-500"
-                                placeholder="Subject"
-                                required
-                            />
-                        </div>
-
-                        <div>
-                            <label className="block text-sm font-medium mb-1">Message</label>
-                            <textarea
-                                name="message"
-                                className="w-full px-3 py-2 border rounded-md focus:ring focus:ring-blue-500"
-                                rows={4}
-                                placeholder="Your Message"
-                                required
-                            ></textarea>
-                            <ValidationError
-                                prefix="Message"
-                                field="message"
-                                errors={state.errors}
-                            />
-                        </div>
-
-                        <button
-                            type="submit"
-                            disabled={state.submitting}
-                            className="w-full bg-[#005B96] text-white py-2 px-4 rounded-lg hover:bg-[#005a96df] transition"
-                        >
-                            {state.submitting ? "Sending..." : "Send Message"}
-                        </button>
-
-                        {state.succeeded && (
-                            <p className="text-green-600 mt-2">
-                                ✅ Your message has been sent successfully!
+                        <div class="p-5 bg-white rounded-xl shadow-md hover:shadow-lg transition">
+                            <h2 class="text-xl font-semibold text-[#005B96] mb-2">
+                                Amity University Jaipur Campus
+                            </h2>
+                            <p class="text-gray-600 leading-relaxed">
+                                SP-1, Kant Kalwar, RIICO Industrial Area, NH-11C, Jaipur, Rajasthan <br />
+                                <span class="font-medium text-gray-800">Tel:</span> 01426-405678
+                                <span class="text-sm text-gray-500">(University Board Number - For general queries only)</span>
                             </p>
-                        )}
-                    </form>
+                        </div>
+                        <div class="p-5 bg-white rounded-xl shadow-md hover:shadow-lg transition">
+                            <h2 class="text-xl font-semibold text-[#005B96] mb-2">
+                                Amity University Jaipur City Office
+                            </h2>
+                            <p class="text-gray-600 leading-relaxed">
+                                Amity House, C - 119, Lal Kothi Scheme, Behind Vidhan Sabha, Jaipur - 302 015 (Rajasthan) <br />
+                                <span class="font-medium text-gray-800">Tel:</span> 0141-4735000, 2744350
+                            </p>
+                        </div>
+
+                        <form
+                            onSubmit={handleSubmit}
+                            className="space-y-4 bg-gray-50 p-6 rounded-2xl shadow"
+                        >
+                            <div>
+                                <label className="block text-sm font-medium mb-1">Name</label>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    className="w-full px-3 py-2 border rounded-md focus:ring focus:ring-blue-500"
+                                    placeholder="Your Name"
+                                    required
+                                />
+                            </div>
+
+                            <div>
+                                <label className="block text-sm font-medium mb-1">Email</label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    className="w-full px-3 py-2 border rounded-md focus:ring focus:ring-blue-500"
+                                    placeholder="Your Email"
+                                    required
+                                />
+                                <ValidationError
+                                    prefix="Email"
+                                    field="email"
+                                    errors={state.errors}
+                                />
+                            </div>
+
+                            <div>
+                                <label className="block text-sm font-medium mb-1">Subject</label>
+                                <input
+                                    type="text"
+                                    name="subject"
+                                    className="w-full px-3 py-2 border rounded-md focus:ring focus:ring-blue-500"
+                                    placeholder="Subject"
+                                    required
+                                />
+                            </div>
+
+                            <div>
+                                <label className="block text-sm font-medium mb-1">Message</label>
+                                <textarea
+                                    name="message"
+                                    className="w-full px-3 py-2 border rounded-md focus:ring focus:ring-blue-500"
+                                    rows={4}
+                                    placeholder="Your Message"
+                                    required
+                                ></textarea>
+                                <ValidationError
+                                    prefix="Message"
+                                    field="message"
+                                    errors={state.errors}
+                                />
+                            </div>
+
+                            <button
+                                type="submit"
+                                disabled={state.submitting}
+                                className="w-full bg-[#005B96] text-white py-2 px-4 rounded-lg hover:bg-[#005a96df] transition"
+                            >
+                                {state.submitting ? "Sending..." : "Send Message"}
+                            </button>
+
+                            {state.succeeded && (
+                                <p className="text-green-600 mt-2">
+                                    ✅ Your message has been sent successfully!
+                                </p>
+                            )}
+                        </form>
                     </div>
 
                 </div>
--- components/hero.jsx
+++ components/hero.jsx
@@ -10,10 +10,10 @@ const montserrat_alt = Montserrat_Alternates({
 const Hero = () => {
   return (
     <div className={`w-full min-h-[50vh] md:min-h-[60vh] lg:min-h-[70vh] bg-[url(/bg.svg)] bg-cover bg-no-repeat bg-center flex items-center justify-center ${montserrat_alt.className}`}>
-        <div className='px-4 sm:px-6 flex items-center flex-col gap-y-4 md:gap-y-6'>
-          <div className='flex items-center justify-between gap-14'>
-            <img className='w-[150px]' src="/iset-logo.jpg" alt="International Conference on Intelligent Systems and Emerging Technologies" />
-            <img className='w-[200px]' src="/amity-logo.png" alt="Amity University Rajasthan" />
+        <div className='px-4 sm:px-6 flex items-center flex-col gap-y-4 md:gap-y-6 py-2'>
+          <div className='flex items-center justify-between gap-14 mt-5'>
+            <img className='w-[100px] md:w-[150px]' src="/iset-logo.jpg" alt="International Conference on Intelligent Systems and Emerging Technologies" />
+            <img className='w-[150px] md:w-[200px]' src="/amity-logo.png" alt="Amity University Rajasthan" />
           </div>
             <h1 className='font-bold text-2xl md:text-3xl lg:text-3xl text-center text-[#005B96] px-10'>International Conference on <br />Intelligent Systems and Emerging Technologies - (ISET 2026)</h1>
             <span className='text-center text-lg md:text-xl text-[#005B96]'>Organised By</span>
